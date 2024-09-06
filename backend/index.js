d5c6ef9f2cb39978413a96b5cfc94762ed8f59ba
acteiongithub <admin@acteion.com>
2024-09-06 10:34:59 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-require('dotenv').config();
+require('dotenv').config('../.env');
 
 const app = express();
 const port = 5000;
